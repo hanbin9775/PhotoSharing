22a29860bc775c82c698ea0a3f75331611a7bdb1
이민호 <lkjjr0424@hanmail.net>
2019-11-09 08:18:14 +0900

    final

--- 2019_Summer_intern-master/src/components/Folder.js
+++ 2019_Summer_intern-master/src/components/Folder.js
@@ -3,30 +3,16 @@ import GroupInfo from "./GroupInfo";
 import styled, { keyframes } from "styled-components";
 
 class Folder extends Component {
-  static defaultProps = {
-    info: {
-      name: this.name,
-      password: this.password
-    }
-  };
-
-  state = {
-    name: this.name,
-    password: this.password
-  };
-
   render() {
     const style = {
       padding: "8px",
       margin: "8px"
     };
 
-    // 일반모드
-    const { name, password } = this.props.info;
     return (
       <img
-        src={this.state.url || "http://via.placeholder.com/400x300"}
-        // src={require("./images/animatedcamera.gif")}
+        src={"./images/KakaoTalk_20191109_075625700.jpg"}
+        // src={require("./images/KakaoTalk_20191109_075625700.jpg")}
         alt="업로드 하기 전입니당"
         width="400"
       />
